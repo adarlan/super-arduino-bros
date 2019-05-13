d62bf9a0cc62efdc45c994cde70c5d574639bfbc
Adarlan Teixeira <adarlan18@gmail.com>
2019-05-12 21:54:23 -0300

    Update code

--- super-arduino-bros.ino
+++ super-arduino-bros.ino
@@ -1,18 +1,26 @@
 
 int C  = 0;
-int Cs = 1;  int Db = Cs;
+int Cs = 1;
 int D  = 2;
-int Ds = 3;  int Eb = Ds;
+int Ds = 3;
 int E  = 4;
 int F  = 5;
-int Fs = 6;  int Gb = Fs;
+int Fs = 6;
 int G  = 7;
-int Gs = 8;  int Ab = Gs;
+int Gs = 8;
 int A  = 9;
-int As = 10; int Bb = As;
+int As = 10;
 int B  = 11;
 
-float noteFrequencies[] = {
+int Db = Cs;
+int Eb = Ds;
+int Gb = Fs;
+int Ab = Gs;
+int Bb = As;
+
+int X = -1;
+
+float frequencies[] = {
 	2112.00000000000f,
 	2237.58606720000f,
 	2370.63987127252f,
@@ -27,237 +35,222 @@ float noteFrequencies[] = {
 	3986.92533026287f
 };
 
-int notePins[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-int buzzerPin = A0;
+int leds[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+int buzzer = A0;
 
 int bpm;
 float R;
 
-void setBpm(int _bpm) {
-	bpm = _bpm;
+void setBpm(int value) {
+	bpm = value;
 	R = 60000.0f / (float) bpm;
 }
 
 void play(int note, float duration) {
-	
-	int _duration = (int) round(duration);
 
-	if (note > -1) {
+	int intDuration = (int) round(duration);
+
+	if (note != X) {
 
 		for (int i = 0; i < 12; i++) {
-			if (i == note) digitalWrite(notePins[i], HIGH);
-			else digitalWrite(notePins[i], LOW);
+			if (i == note) digitalWrite(leds[i], HIGH);
+			else digitalWrite(leds[i], LOW);
 		}
-		
-		int _frequency = (int) round(noteFrequencies[note]);
-		tone(buzzerPin, _frequency, _duration);
+
+		int frequency = (int) round(frequencies[note]);
+		tone(buzzer, frequency, intDuration);
 	}
 
-	delay(_duration);
-	noTone(buzzerPin);
+	delay(intDuration);
+	noTone(buzzer);
 }
 
 void setup() {
 
-	pinMode(buzzerPin, OUTPUT);
-	for (int i = 0; i < 12; i++) pinMode(notePins[i], OUTPUT);
+	pinMode(buzzer, OUTPUT);
 
+	for (int i = 0; i < 12; i++) {
+		pinMode(leds[i], OUTPUT);
+	}
+}
+
+void loop() {
 	overworld();
 	deathSound();
-
-	for (int i = 0; i < 12; i++) pinMode(notePins[i], INPUT);
 }
 
 void deathSound() {
 
 	setBpm(100);
 
-	play(C,  R/16.0f);
-	play(Cs, R/16.0f);
-	play(D,  R/8.0f);
-	play(-1, R/4.0f);
-	play(-1, R/2.0f);
+	play(C, R/16.0f);
+	play(Cs,R/16.0f);
+	play(D, R/8.0f);
+	play(X, R/4.0f);
+	play(X, R/2.0f);
 
-	play(B,  R/4.0f);
-	play(F,  R/4.0f);
-	play(-1, R/4.0f);
-	play(F,  R/4.0f);
+	play(B, R/4.0f);
+	play(F, R/4.0f);
+	play(X, R/4.0f);
+	play(F, R/4.0f);
 
-	play(F,  R/3.0f);
-	play(E,  R/3.0f);
-	play(D,  R/3.0f);
+	play(F, R/3.0f);
+	play(E, R/3.0f);
+	play(D, R/3.0f);
 
-	play(C,  R/4.0f);
-	play(E,  R/4.0f);
-	play(E,  R/4.0f);
-	play(E,  R/4.0f);
+	play(C, R/4.0f);
+	play(E, R/4.0f);
+	play(E, R/4.0f);
+	play(E, R/4.0f);
 
-	play(C,  R/4.0f);
-	play(-1, R/4.0f);
-	play(-1, R/2.0f);
+	play(C, R/4.0f);
+	play(X, R/4.0f);
+	play(X, R/2.0f);
 }
 
 void overworld() {
 
 	setBpm(100);
-	
-	overworld_parte_1();
-	overworld_parte_2();
-	overworld_parte_2();
-	overworld_parte_3();
-	overworld_parte_3();
+
+	overworld_1();
+	overworld_2();
+	overworld_2();
+	overworld_3();
+	overworld_3();
 }
 
-void overworld_parte_1() {
+void overworld_1() {
 
-	play(E,  R/4.0f);
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
-	play(E,  R/4.0f);
+	play(E, R/4.0f);
+	play(E, R/4.0f);
+	play(X, R/4.0f);
+	play(E, R/4.0f);
 
-	play(-1, R/4.0f);
-	play(C,  R/4.0f);
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
+	play(X, R/4.0f);
+	play(C, R/4.0f);
+	play(E, R/4.0f);
+	play(X, R/4.0f);
 
-	play(G,  R/4.0f);
-	play(-1, R/4.0f);
-	play(-1, R/2.0f);
+	play(G, R/4.0f);
+	play(X, R/4.0f);
+	play(X, R/2.0f);
 
-	play(G,  R/4.0f);
-	play(-1, R/4.0f);
-	play(-1, R/2.0f);
+	play(G, R/4.0f);
+	play(X, R/4.0f);
+	play(X, R/2.0f);
 }
 
-void overworld_parte_2() {
+void overworld_2() {
 
-	play(C,  R/4.0f);
-	play(-1, R/4.0f);
-	play(-1, R/4.0f);
-	play(G,  R/4.0f);
+	play(C, R/4.0f);
+	play(X, R/4.0f);
+	play(X, R/4.0f);
+	play(G, R/4.0f);
 
-	play(-1, R/2.0f);
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
+	play(X, R/2.0f);
+	play(E, R/4.0f);
+	play(X, R/4.0f);
 
-	play(-1, R/4.0f);
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
-	play(B,  R/4.0f);
+	play(X, R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
+	play(B, R/4.0f);
 
-	play(-1, R/4.0f);
-	play(Bb, R/4.0f);
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
+	play(X, R/4.0f);
+	play(Bb,R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
 
-	play(G,  R/3.0f);
-	play(E,  R/3.0f);
-	play(G,  R/3.0f);
+	play(G, R/3.0f);
+	play(E, R/3.0f);
+	play(G, R/3.0f);
 
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
-	play(F,  R/4.0f);
-	play(G,  R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
+	play(F, R/4.0f);
+	play(G, R/4.0f);
 
-	play(-1, R/4.0f);
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
-	play(C,  R/4.0f);
+	play(X, R/4.0f);
+	play(E, R/4.0f);
+	play(X, R/4.0f);
+	play(C, R/4.0f);
 
-	play(D,  R/4.0f);
-	play(B,  R/4.0f);
-	play(-1, R/2.0f);
+	play(D, R/4.0f);
+	play(B, R/4.0f);
+	play(X, R/2.0f);
 }
 
-void overworld_parte_3() {
+void overworld_3() {
 
-	overworld_parte_3_a();
-	overworld_parte_3_b();
+	overworld_3_a();
+	overworld_3_b();
 
-	overworld_parte_3_a();
-	overworld_parte_3_c();
+	overworld_3_a();
+	overworld_3_c();
 
-	overworld_parte_3_a();
-	overworld_parte_3_b();
+	overworld_3_a();
+	overworld_3_b();
 
-	overworld_parte_3_d();
+	overworld_3_d();
 }
 
-void overworld_parte_3_a() {
+void overworld_3_a() {
 
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
-	play(G,  R/4.0f);
-	play(Gb, R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
+	play(G, R/4.0f);
+	play(Gb,R/4.0f);
 
-	play(F,  R/4.0f);
-	play(Ds, R/4.0f);
-	play(A,  R/4.0f);
-	play(E,  R/4.0f);
+	play(F, R/4.0f);
+	play(Ds,R/4.0f);
+	play(A, R/4.0f);
+	play(E, R/4.0f);
 }
 
-void overworld_parte_3_b() {
+void overworld_3_b() {
 
-	play(D,  R/4.0f);
-	play(G,  R/4.0f);
-	play(A,  R/4.0f);
-	play(C,  R/4.0f);
+	play(D, R/4.0f);
+	play(G, R/4.0f);
+	play(A, R/4.0f);
+	play(C, R/4.0f);
 
-	play(A,  R/4.0f);
-	play(A,  R/4.0f);
-	play(C,  R/4.0f);
-	play(E,  R/4.0f);
+	play(A, R/4.0f);
+	play(A, R/4.0f);
+	play(C, R/4.0f);
+	play(E, R/4.0f);
 }
 
-void overworld_parte_3_c() {
+void overworld_3_c() {
 
-	play(-1, R/4.0f);
-	play(C,  R/4.0f);
-	play(-1, R/4.0f);
-	play(C,  R/4.0f);
+	play(X, R/4.0f);
+	play(C, R/4.0f);
+	play(X, R/4.0f);
+	play(C, R/4.0f);
 
-	play(C,  R/4.0f);
-	play(-1, R/4.0f);
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
+	play(C, R/4.0f);
+	play(X, R/4.0f);
+	play(E, R/4.0f);
+	play(X, R/4.0f);
 }
 
-void overworld_parte_3_d() {
+void overworld_3_d() {
 
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
-	play(Eb, R/4.0f);
-	play(-1, R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
+	play(Eb,R/4.0f);
+	play(X, R/4.0f);
 
-	play(-1, R/4.0f);
-	play(D,  R/4.0f);
-	play(-1, R/2.0f);
+	play(X, R/4.0f);
+	play(D, R/4.0f);
+	play(X, R/2.0f);
 
-	play(C,  R/4.0f);
-	play(-1, R/4.0f);
-	play(-1, R/4.0f);
-	play(E,  R/4.0f);
+	play(C, R/4.0f);
+	play(X, R/4.0f);
+	play(X, R/4.0f);
+	play(E, R/4.0f);
 
-	play(E,  R/4.0f);
-	play(-1, R/4.0f);
-	play(A,  R/4.0f);
-	play(-1, R/4.0f);
-}
-
-void loop() {
-
-	boolean buzzing = false;
-
-	for (int i = 0; i < 12; i++) {
-
-		if (digitalRead(notePins[i]) == HIGH) {
-			buzzing = true;
-			tone(buzzerPin, noteFrequencies[i]);
-		}
-	}
-
-	if (!buzzing) {
-		noTone(buzzerPin);
-	}
-
+	play(E, R/4.0f);
+	play(X, R/4.0f);
+	play(A, R/4.0f);
+	play(X, R/4.0f);
 }
